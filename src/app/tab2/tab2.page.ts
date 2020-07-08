aa44ec196545dfb3ee034ae7e8816fb1a8cbcf18
dlara95 <dayanalara95@gmail.com>
2020-07-07 21:01:03 -0600

    Account component

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,4 +1,9 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators, FormControlName } from '@angular/forms';
+import { Router } from '@angular/router';
+import { NavController, ToastController } from '@ionic/angular';
+import { RestService } from '../rest.service';
+import { StorageproviderService } from '../storageprovider.service';
 
 @Component({
   selector: 'app-tab2',
@@ -6,7 +11,109 @@ import { Component } from '@angular/core';
   styleUrls: ['tab2.page.scss']
 })
 export class Tab2Page {
+  name: FormControl;
+  email: FormControl;
+  birthdate: FormControl;
+  password: FormControl;
+  confirmPassword: FormControl;
+  usertype: FormControl;
+  updateForm: FormGroup;
+  regex:string='(?=.*)(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9_]).{8,20}$';
+  hideButton:boolean = false;
+  userid:any;
+  validation_messages = {
+    'field': [
+        { type: 'required', message: 'Field is required.' },
+        { type: 'minlength', message: 'Field must be at least 5 characters long.' },
+        { type: 'maxlength', message: 'Field cannot be more than 25 characters long.' },
+        { type: 'pattern', message: 'Password needs: 1 uppercase, 1 number, Min 8 characters.' },
+        { type: 'validUsername', message: 'Your Field has already been taken.' },
+        { type: 'email', message: 'Your email is not valid' }
+      ]
+    
+    }
+  constructor(private router: Router, private navCtrl: NavController, private rest: RestService,
+    public toastController: ToastController, private storageProvider: StorageproviderService) {}
 
-  constructor() {}
+   async ngOnInit(){
+      this.createFormControls();
+      this.createupdateForm();
+      await this.getUserInfo();
+    }
+
+    getUserInfo(){
+      return new Promise((resolve, reject) => {
+        this.storageProvider.get('userid').then(res => {
+          this.userid = res;
+          console.log("userid", res, this.userid);
+        })
+      })
+    }
+    createFormControls(){
+      this.name = new FormControl('', [Validators.required]); 
+      this.email = new FormControl('', [Validators.required, Validators.email]); 
+      this.birthdate = new FormControl('');
+    }
+  
+    createupdateForm(){
+      this.updateForm = new FormGroup({
+        name: this.name,
+        email: this.email,
+        birthdate: this.birthdate,
+      }, {
+        updateOn: 'submit'
+      })
+    }
+
+
+    //custom validation for each form field on DOM
+  validateAllFormFields(formGroup: FormGroup) {         
+    Object.keys(formGroup.controls).forEach(field => { 
+      const control = formGroup.get(field);             
+      if (control instanceof FormControl) {             
+        control.markAsTouched({ onlySelf: true });
+      } else if (control instanceof FormGroup) {        
+        this.validateAllFormFields(control);  
+      }
+    });
+  }
+
+  update(){
+    this.hideButton = true;
+    this.updateForm.controls['email'].setValue(this.updateForm.get('email').value.trim());
+    console.log("value", this.updateForm.value, this.updateForm.valid);
+    if(this.updateForm.valid){
+     this.sendData();
+    }else{
+      this.validateAllFormFields(this.updateForm);
+      this.hideButton = false;
+    }
+  }
+
+  sendData(){
+    console.log("What to send")
+    this.rest.sendData("/user/" + this.userid , this.updateForm.value).subscribe(val => {
+      if(val.hasOwnProperty('message')){
+        this.presentToast(val['message']);
+        this.hideButton = false;
+      }else{
+       
+        this.presentToast("An error ocurred");
+      }
+   
+    }, error => {
+      this.presentToast(error);
+      this.hideButton = false;
+
+    })
+  }
+
+  async presentToast(msg) {
+    const toast = await this.toastController.create({
+      message: msg,
+      duration: 2000
+    });
+    toast.present();
+  }
 
 }
